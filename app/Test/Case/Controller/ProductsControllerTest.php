48610bb113a1aa76feda8df64e3dc7e84e252617
kazuhirosaji <kazuhirosaji@gmail.com>
2014-10-31 20:25:30 -0700

    ProductsController index test

--- app/Test/Case/Controller/ProductsControllerTest.php
+++ app/Test/Case/Controller/ProductsControllerTest.php
@@ -23,6 +23,35 @@ class ProductsControllerTest extends ControllerTestCase {
  * @return void
  */
 	public function testIndex() {
+		$result = $this->testAction('/products/index', array('method' => 'get', 'return' => 'vars'));
+		debug($result);
+
+		$expected = array(
+				array('Product' => array(
+				'id' => '1',
+				'name' => 'Sake',
+				'nation_id' => '1',
+				'description' => 'Sake is alchol drink in Japan.',
+				'link' => 'http://www.yahoo.co.jp',
+				'image' => '',
+				'created' => '2014-10-18 13:58:59',
+				'modified' => '2014-10-18 13:58:59'),
+				'Nation' => array("id" => 1, "name" => "JPN"),
+			),
+				array('Product' => array(
+				'id' => '2',
+				'name' => 'Beer',
+				'nation_id' => '2',
+				'description' => 'Beer is famous alchol drink in German.',
+				'link' => 'http://www.yahoo.co.jp',
+				'image' => '',
+				'created' => '2014-10-29 13:58:59',
+				'modified' => '2014-10-29 13:58:59'),
+				'Nation' => array("id" => 2, "name" => "USA"),
+			),
+			);
+
+		$this->assertEquals($expected, $result['products']);
 	}
 
 /**
